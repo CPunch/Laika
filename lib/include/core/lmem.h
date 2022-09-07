674ea2b47b9796f336d1fdd3652c347a206cb5a6
CPunch <sethtstubbs@gmail.com>
2022-09-07 16:53:14 -0500

    lmem.[ch]: removed inlined functions
    - fixes some builds

--- lib/include/core/lmem.h
+++ lib/include/core/lmem.h
@@ -57,29 +57,7 @@
     } while (0);
 
 void *laikaM_realloc(void *buf, size_t sz);
-
-inline bool laikaM_isBigEndian(void)
-{
-    union
-    {
-        uint32_t i;
-        uint8_t c[4];
-    } _indxint = {0xDEADB33F};
-
-    return _indxint.c[0] == 0xDE;
-}
-
-inline void laikaM_reverse(uint8_t *buf, size_t sz)
-{
-    int k;
-
-    /* swap bytes, reversing the buffer */
-    for (k = 0; k < (sz / 2); k++) {
-        uint8_t tmp = buf[k];
-        buf[k] = buf[sz - k - 1];
-        buf[sz - k - 1] = tmp;
-    }
-}
-
+bool laikaM_isBigEndian(void);
+void laikaM_reverse(uint8_t *buf, size_t sz);
 
 #endif
--- lib/src/core/lmem.c
+++ lib/src/core/lmem.c
@@ -18,3 +18,26 @@ void *laikaM_realloc(void *buf, size_t sz)
 
     return newBuf;
 }
+
+bool laikaM_isBigEndian(void)
+{
+    union
+    {
+        uint32_t i;
+        uint8_t c[4];
+    } _indxint = {0xDEADB33F};
+
+    return _indxint.c[0] == 0xDE;
+}
+
+void laikaM_reverse(uint8_t *buf, size_t sz)
+{
+    int k;
+
+    /* swap bytes, reversing the buffer */
+    for (k = 0; k < (sz / 2); k++) {
+        uint8_t tmp = buf[k];
+        buf[k] = buf[sz - k - 1];
+        buf[sz - k - 1] = tmp;
+    }
+}
